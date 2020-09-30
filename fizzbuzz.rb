dfe506a0650d10668e3de1bf7f3404464b5de81d
Mark G <markghaida@gmail.com>
2020-09-30 04:19:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz (number) 
-  if number % 3 == 0
-     return "Fizz"
+  if (number % 3 == 0) && (number % 5 == 0)
+     return "FizzBuzz"
+     
+  else if number % 5 == 0 
+    return "Buzz"
+    
+  else number % 3 == 0
+    return "Fizz"
   end
 end
   
