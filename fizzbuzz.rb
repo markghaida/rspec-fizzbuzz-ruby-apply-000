c5b49985d88a0057e5b4e22ffd86547f0e77519f
Mark G <markghaida@gmail.com>
2020-09-30 04:24:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,10 @@ def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
       "FizzBuzz"
      
-  else if number % 5 == 0 
+  elsif number % 5 == 0 
       "Buzz"
       
-  else if number % 3 == 0
+  elsif number % 3 == 0
       "Fizz"
   else 
     nil
